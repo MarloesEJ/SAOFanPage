0fbc4b39567d91164d0dc5a039558e93cbf4ffd5
RavenMQueen <marloes1998@gmail.com>
2024-02-11 23:04:56 +0100

    added hamburger menu, make sure h3 in header disapears when you open menu.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,29 @@
 const popup = document.getElementById('js--popup')
 const overlay = document.getElementById('overlay_popup')
+const hamburger = document.getElementById('js--hamburger')
+const navBar = document.getElementById('js--navbar')
+const navBlocks = document.getElementById('js--navbar-links')
+const overlayH3 = document.getElementById('js--H3')
+let stateNav = 0
+
+hamburger.addEventListener("click", nav)
+
+function nav(){
+  if (stateNav == 0){
+    hamburger.classList.add("active")
+    navBar.classList.add("active")
+    navBlocks.classList.add("active")
+    overlayH3.classList.add('active')
+    stateNav = 1
+  } 
+  else{
+    hamburger.classList.remove("active")
+    navBar.classList.remove("active")
+    navBlocks.classList.remove("active")
+    stateNav = 0
+  }
+
+}
 
 function openOrder(){
   window.location.href = "orderform.html"
